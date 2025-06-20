4decc6aaf4fa86a12b9e63477788d5f9233a918a
Bellam-Saikumar <139708278+Bellam-Saikumar@users.noreply.github.com>
2025-06-20 23:15:15 +0530

    Update AppDownload.jsx

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from "../../assets/assets";
 const AppDownload = ()=>{
     return (
         <div className="app-download"  id="app-download" >
-           <p>Better Experinence Download <br /> Tomato App</p>
+           <p>Better Experinence Download <br /> BookStore App</p>
            <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />
             <img src={assets.app_store} alt="" />
